b385f3b529cab052c8483dbc0ec94340a788fbe2
Ben Lachman <blachman@mac.com>
2018-09-03 12:00:14 -0400

    Update config

--- config.py
+++ config.py
@@ -2,14 +2,15 @@ class Config(object):
     DEBUG = False
     TESTING = False
     MONGODB_DB = 'matchdb'
-    # DATABASE_URI = 'sqlite:///:memory:'
+
 
 class ProductionConfig(Config):
-    HOST = 'http://google.com'
-    # DATABASE_URI = 'mysql://user@localhost/foo'
+    pass
+
 
 class DevelopmentConfig(Config):
     DEBUG = True
+
 
 class TestingConfig(Config):
     TESTING = True
